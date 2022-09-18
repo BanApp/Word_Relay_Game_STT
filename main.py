ec09ad7edfc77946c88f5ab6c88d90dcbb2558a7
minjunjung <jmj284@gmail.com>
2022-09-18 16:36:00 +0900

    음성인식 개선, 인식 시간, 난이도 설정 가능, 랜덤 개선, 가독성 높임

--- main.py
+++ main.py
@@ -4,50 +4,74 @@ from gtts import gTTS
 import os
 import speech_recognition as sr
 import random
+import time
+import os
+from tkinter import *
+
+
+def countdown(num_of_secs):
+    while num_of_secs:
+        s = num_of_secs
+        print(s, end=' ')
+        time.sleep(1)
+        num_of_secs -= 1
+    print('시간종료')
 
 def speak(text ,lang="ko", speed=False):
     tts = gTTS(text=text, lang=lang , slow=speed)
     tts.save("./tts.mp3")
     os.system("afplay " + "./tts.mp3")
 
-
 def etoe(time,level):
     rdr = pd.read_csv('myungsa.csv', engine='pyarrow', index_col=0)
-
     Recognizer = sr.Recognizer()
     mic = sr.Microphone()
-
     tmp = []
     check = {}
     p_key = 'a'
     c_key = 'b'
+    k = 'Y'
+    while 1:
+        with mic as source:
+            audio = Recognizer.record(source, duration=time)
+        try:
+            data = Recognizer.recognize_google(audio_data=audio, language="ko-kr")
+            data = data.replace(" ", "")
+            print("Player: ",data)
+            print('[Y/N]')
 
-    with mic as source:
-        audio = Recognizer.record(source, duration=time)
+            k = sys.stdin.readline().rstrip()
 
-    try:
-        data = Recognizer.recognize_google(audio_data=audio, language="ko-kr")
-        print("Player: ", data)
-    except sr.UnknownValueError:
-        print("Your speech can not understand")
-    except sr.RequestError as e:
-        print("Request Error!; {0}".format(e))
+            if k == 'Y':
+                break
+            else:
+                print("다시 한번 말씀하세요!")
+
+        except sr.UnknownValueError:
+            print("시간초과 입니다.")
+            speak("시간초과 입니다.")
+            break
+
+        except sr.RequestError as e:
+            print("Request Error!; {0}".format(e))
+            break
 
     # data = sys.stdin.readline().rstrip() #텍스트 직접 입력
 
     ck = 0
     for i in rdr.index:
+        i = i.replace(" ", "")
         if i == data and len(i) > 1:
             p_key = data[-1]
             tmp.append(data)
-            print("Player:", i)
-            print('<pass>')
+            print('[Pass]')
+            print()
             ck = 1
             break
 
     if ck == 0:
-        print("한글자 단어는 사용할 수 없습니다. 규칙에 어긋납니다.")
-        speak("한글자 단어는 사용할 수 없습니다. 규칙에 어긋납니다.")
+        print("규칙에 어긋납니다.")
+        speak("규칙에 어긋납니다.")
         exit(0)
 
     while 1:
@@ -56,6 +80,7 @@ def etoe(time,level):
             ck = 3
             rd = []
             for j in rdr.index:
+                j = j.replace(" ", "")
                 if (j[0] == p_key) and (len(j) > 1) and (j not in tmp):
                     rd.append(j)
 
@@ -67,7 +92,8 @@ def etoe(time,level):
 
             if check[x[0]] <= level:
                 print("Computer:", x)
-                print('<pass>')
+                print('[Pass]')
+                print()
                 speak(x)
                 tmp.append(x)
                 c_key = x[-1]
@@ -77,15 +103,29 @@ def etoe(time,level):
 
         elif ck == 0:
             # 사람 순서
-            with mic as source:
-                audio = Recognizer.record(source, duration=time)
-            try:
-                data = Recognizer.recognize_google(audio_data=audio, language="ko-kr")
-                print("Player: ", data)
-            except sr.UnknownValueError:
-                print("Your speech can not understand")
-            except sr.RequestError as e:
-                print("Request Error!; {0}".format(e))
+            while 1:
+                with mic as source:
+                    audio = Recognizer.record(source, duration=time)
+                try:
+                    data = Recognizer.recognize_google(audio_data=audio, language="ko-kr")
+                    data = data.replace(" ", "")
+                    print("Player: ", data)
+                    print('[Y/N]')
+                    k = sys.stdin.readline().rstrip()
+
+                    if k == 'Y':
+                        break
+                    else:
+                        print("다시 한번 말씀하세요!")
+
+                except sr.UnknownValueError:
+                    print("시간초과 입니다.")
+                    speak("시간초과 입니다.")
+                    break
+
+                except sr.RequestError as e:
+                    print("Request Error!; {0}".format(e))
+                    break
 
             # data = sys.stdin.readline().rstrip() #텍스트 직접 입력
 
@@ -93,24 +133,44 @@ def etoe(time,level):
             for i in rdr.index:
                 if (i == data) and (len(i) > 1) and (data not in tmp) and (data[0] == c_key):
                     p_key = data[-1]
-                    print('<pass>')
+                    print('[Pass]')
+                    print()
                     ck = 1
                     break
 
         elif ck == 2:
+            #컴퓨터 승리
             print('=========END========')
             print('====Computer WIN====')
             print('=====================')
             speak('Computer WIN')
-            exit(0)
+            print("다시 하시겠습니까?[Y/N]")
+            p = sys.stdin.readline().rstrip()
+            if p == 'Y':
+                return 1
+            else:
+                print('프로그램을 종료 합니다.')
+                exit(0)
 
         elif ck == 3:
+            #사람 승리
             print('=========END=========')
             print('======Human WIN======')
             print('======================')
             speak('Human WIN')
-            exit(0)
+            print("다시 하시겠습니까?[Y/N]")
+            p = sys.stdin.readline().rstrip()
+            if p == 'Y':
+                return 1
+            else:
+                print('프로그램을 종료 합니다.')
+                exit(0)
 
-print("지속 시간과 난이도를 입력해주세요:")
-x,y = map(int,sys.stdin.readline().split())
-etoe(x,y)
+def first():
+    while 1:
+        print("<음성 인식 지속 시간과 난이도를 입력해주세요>")
+        x, y = map(int, sys.stdin.readline().split())
+        etoe(x,y)
+
+first()
+
